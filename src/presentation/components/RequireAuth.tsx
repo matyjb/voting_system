96917c95d81219b889fa0c5cc5b2e3e70c4074bb
matyjb <matyjb14@gmail.com>
2024-04-04 21:59:02 +0200

    spinner when firebase auth is loading

--- src/presentation/components/RequireAuth.tsx
+++ src/presentation/components/RequireAuth.tsx
@@ -2,14 +2,33 @@ import { FunctionComponent } from "react";
 import { auth } from "../../logic/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
+import { Layout, Spin } from "antd";
+import { Content } from "antd/es/layout/layout";
 
 interface RequireAuthProps {
   children: JSX.Element;
 }
 
 const RequireAuth: FunctionComponent<RequireAuthProps> = ({ children }) => {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const location = useLocation();
+
+  if (loading) {
+    return (
+      <Layout>
+        <Content
+          style={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Spin size="large" />
+        </Content>
+      </Layout>
+    );
+  }
 
   return user ? (
     children
